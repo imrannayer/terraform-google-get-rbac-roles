aec0910f491d9a255f27f4312bc4aa73a79836f6
Imran Nayer <imrannayer@google.com>
2023-04-17 11:45:36 -0500

    updated dbs role

--- job-roles.tf
+++ job-roles.tf
@@ -24,12 +24,11 @@ locals {
       "roles/storage.objectViewer",
     ]
     "dba" = [
-      "roles/dataflow.admin",
       "roles/dataproc.admin",
       "roles/composer.admin",
       "roles/storage.objectViewer",
       "roles/bigquery.user",
-      "roles/dataflow.developer",
+      # "roles/dataflow.developer",
     ]
     "developer" = [
       "roles/dataflow.developer",
